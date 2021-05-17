b9a8035ddeb444a62ec2f63425d483ab69635e14
이석호 <dldlsh2309@gmail.com>
2021-05-17 18:02:45 +0900

    fix : 실행 application 단일화

--- backend/FarmDiary_Board_Service/src/main/java/com/ssafy/test/model/ReplyRepository.java
+++ backend/FarmDiary_Board_Service/src/main/java/com/ssafy/test/model/ReplyRepository.java
@@ -2,9 +2,10 @@ package com.ssafy.test.model;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ReplyRepository extends PagingAndSortingRepository<Reply, String>, CustomRepository {
+public interface ReplyRepository extends MongoRepository<Reply, String>, CustomRepository {
 
 	List<Reply> findByBoardId(String boardId);
 }
